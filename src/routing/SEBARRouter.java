1625cb6ae9f32aa2b12f66a1c8710a591934d975
Victor Roque <vmr1@icomp.ufam.edu.br>
2019-06-18 01:14:00 -0400

    Funções de calculo adicionadas

--- src/routing/SEBARRouter.java
+++ src/routing/SEBARRouter.java
@@ -17,6 +17,8 @@ import routing.community.CWindowCentrality;
  */
 public class SEBARRouter extends ActiveRouter {
 
+    private static final int ENERGIA_CONSUMIDA = 1;
+    private static final double PORC_ENERGIA = 0.9;
     private KCliqueCommunityDetection comunidade;
     private CWindowCentrality centralidade;
 
@@ -27,6 +29,18 @@ public class SEBARRouter extends ActiveRouter {
 	protected SEBARRouter(SEBARRouter r) {
 		super(r);
 	}
+
+    private double calculoEnk(DTNHost peer) {
+        return (1 - PORC_ENERGIA) * (ENERGIA_CONSUMIDA / 2);
+    }
+
+    private double calculoEk(DTNHost peer) {
+        return calculoEnk(peer) + calculoEck(peer);
+    }
+
+    private double calculoEck(DTNHost peer) {
+        return 0.7;
+    }
 
 	@Override
 	public void update() {
